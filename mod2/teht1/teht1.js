f62010f672bbf9de3b339d90d0e99878aac17ec5
Eero Pokki <eero.pokki@gmail.com>
2025-11-17 21:17:30 +0200

    mod 2 teht 1

---
+++ mod2/teht1/teht1.js
@@ -0,0 +1,13 @@
+'use script';
+
+let numbers = [];
+
+for (let i = 0; i < 5; i++) {
+  let num = prompt('Anna numero: ')
+  numbers.push(num)
+}
+
+console.log('Numerot päinvastaisessa järjestyksessä.')
+for (let i = numbers.length -1; i >= 0; i--){
+  console.log(numbers[i])
+}
