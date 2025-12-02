eece7303955cffd4342b111288b0bfe2cd937776
Eero Pokki <eero.pokki@gmail.com>
2025-12-02 19:09:57 +0200

    mod 2 teht 6 .html ja .js

---
+++ mod2/teht6/teht6.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function rollDice() {
+  return Math.floor(Math.random() * 6 + 1);
+}
+
+let tulos = 0;
+
+while (tulos !== 6) {
+  tulos = rollDice();
+  document.querySelector('#target').innerHTML += `<li>${tulos}</li>`;
+}
